0a48e8b6434ffe3807575a1ace76a83f0c87f418
Julien Taret <julien@profil-web.fr>
2025-08-18 12:15:18 +0200

    Initialize Boilerplate project structure with essential files and configurations

---
+++ Boilerplate/BoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerplateApp.swift
+//  Boilerplate
+//
+//  Created by Julien TARET on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoilerplateTests/BoilerplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerplateTests.swift
+//  BoilerplateTests
+//
+//  Created by Julien TARET on 18/08/2025.
+//
+
+import Testing
+@testable import Boilerplate
+
+struct BoilerplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
